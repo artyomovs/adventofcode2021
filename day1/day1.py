f1667da60367457ca2475df17cd15d7b4b50e068
Sergei Artyomov <artyomov.sergey1989@gmail.com>
2021-12-18 11:03:01 +0300

    Day 1

---
+++ day1/day1.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+# Load inputs
+inputs = []
+for i in Path('input.txt').read_text().split():
+    inputs.append(int(i))
+
+# Part one
+increased = 0
+for i in range(1, len(inputs)):
+    if int(inputs[i]) >= int(inputs[i-1]):
+        increased += 1
+
+print(increased)
+
+# Part two
+increased = 0
+for i in range(0, len(inputs) - 3):
+    if sum(inputs[i:i+3]) < sum(inputs[i+1:i+4]):
+        increased += 1
+
+print(increased)
