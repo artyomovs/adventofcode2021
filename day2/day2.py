5c017fada79b053cb37d8eeb10bf63bc2231e4b8
artyomovs <artyomov.sergey1989@gmail.com>
2022-01-18 12:40:09 +0300

    Day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -7,6 +7,7 @@ inputs = []
 for line in Path('input.txt').read_text().split('\n'):
     inputs.append(tuple(line.split(' ')))
 
+# Part one
 for direction in inputs:
     if direction[0] == 'forward':
         position[0] += int(direction[1])
@@ -14,7 +15,24 @@ for direction in inputs:
         position[1] -= int(direction[1])
     if direction[0] == 'down':
         position[1] += int(direction[1])
-    print (str(direction) + ' ' + str(position))
+    # print (str(direction) + ' ' + str(position))
 
-multiply = position[0] * position[1]
-print(multiply)
+result = position[0] * position[1]
+print(f"Day 2 part 1: {result}")
+
+# Part two
+aim = 0
+for i in inputs:
+    direction = i[0]
+    value = int(i[1])
+    if direction == 'forward':
+        position[0] += value
+        position[1] = position[1] + aim * value
+    if direction == 'down':
+        aim += value
+    if direction == 'up':
+        aim -= value
+    # print(str(direction) + ' ' + str(position))
+
+result = position[0] * position[1]
+print(f"Day 2 part 2: {result}")
